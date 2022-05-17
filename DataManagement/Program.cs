44eaa0339add81601a83fb14df9dd02fa1e7673f
Dawid Kopecki <S00212387@mail.itsligo.ie>
2022-05-17 10:08:25 +0100

    Created code for EF Code First DB Creation

---
+++ DataManagement/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OODMay2022;
+namespace DataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RentData db = new RentData();
+
+            using (db)
+            {
+                RentalProperty p1 = new RentalProperty()
+                {
+                    Price = 400,
+                    TypeOfRental = RentalType.Flat,
+                    Description = "A modern 1 bedroom apartment located close to the campus." +
+                    "  The accomodation comprises of 1 en-suite bedroom with a study area, " +
+                    "a small kitchen and a lounge/dining room",
+                    Location = "Town Centre"
+                };
+
+                RentalProperty p2 = new RentalProperty()
+                {
+                    Price = 400,
+                    TypeOfRental = RentalType.House,
+                    Description = "A modern 4 bedroom townhouse located 2 km from the campus. " +
+                    "The house has 4 large double bedrooms with ample space for a desk, " +
+                    "a large kitchen with all mod cons.  " +
+                    "There is also a dining room and a large sitting room",
+                    Location = "Ballinode"
+                };
+
+                RentalProperty p3 = new RentalProperty()
+                {
+                    Price = 400,
+                    TypeOfRental = RentalType.Share,
+                    Description = "1 bedroom available to share in a 3 bedroom house located in the " +
+                    "beautiful seaside village of Strandhill.  The property is located on the bus route to " +
+                    "IT Sligo with regular buses to and from the campus",
+                    Location = "Strandhill"
+                };
+
+                Console.WriteLine("Rental Properties have been created");
+
+                db.RentalProperties.Add(p1);
+                db.RentalProperties.Add(p2);
+                db.RentalProperties.Add(p3);
+
+                Console.WriteLine("Rental Properties have been added to the table");
+
+                db.SaveChanges();
+                Console.WriteLine("Data has been saved to DB");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
--- OODMay2022/RentalProperty.cs
+++ OODMay2022/RentalProperty.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace OODMay2022
 {
     public class RentalProperty
     {
+        public RentalType TypeOfRental;
+
         public int ID { get; set; }
         public string Location { get; set; }
         public decimal Price { get; set; }
@@ -17,5 +20,12 @@ namespace OODMay2022
     public enum RentalType
     {
         House, Flat, Share
+    }
+
+    public class RentData:DbContext
+    {
+        public RentData() : base("MyPropertyData") { }
+
+        public DbSet <RentalProperty> RentalProperties { get; set; }
     }
 }
