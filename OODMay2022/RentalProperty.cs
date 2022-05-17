0a32f8375f7b179288a12aaa6859dd581a6a4a88
Dawid Kopecki <S00212387@mail.itsligo.ie>
2022-05-17 09:40:19 +0100

    Added class and the properties

---
+++ OODMay2022/RentalProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODMay2022
+{
+    public class RentalProperty
+    {
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        
+    }
+    public enum RentalType
+    {
+        House, Flat, Share
+    }
+}
